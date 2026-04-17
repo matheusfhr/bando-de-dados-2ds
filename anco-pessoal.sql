e1c073903e01ef9d2d720ce3b226f998e0fdcbef
matheusfhr <matheus.rosa02@escola.pr.gov.br>
2026-04-17 11:19:47 -0300

    Update anco-pessoal.sql

--- anco-pessoal.sql
+++ anco-pessoal.sql
@@ -1,1 +1,8 @@
+CREATE TABLE Mercadorias (
+    ID_Referencia INT PRIMARY KEY,
+    Descricao_Item VARCHAR(100) NOT NULL,
+    Valor_Por_Quilo DECIMAL(10, 2) NOT NULL,
+    Informacoes_Extras TEXT
+);
 
+SELECT * FROM Mercadorias;
