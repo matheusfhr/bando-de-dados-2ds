b80421b56c45ce1d8315d94b9dc804cb7daa53ec
matheusfhr <matheus.rosa02@escola.pr.gov.br>
2026-04-17 11:39:30 -0300

    Update bandodads.sql

--- bandodads.sql
+++ bandodads.sql
@@ -1,1 +1,19 @@
+- Criação das tabelas
+CREATE TABLE Alunos (
+    id_aluno INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    data_nascimento DATE
+);
 
+CREATE TABLE Professores (
+    id_professor INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    especialidade TEXT
+);
+
+CREATE TABLE Disciplinas (
+    id_disciplina INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_disciplina TEXT NOT NULL,
+    id_professor INTEGER,
+    FOREIGN KEY (id_professor) REFERENCES Professores(id_professor)
+);
